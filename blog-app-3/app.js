045904875c2f8f28d72cb2310251894bee62e2f9
KaranSingh1301 <70521663+KaranSingh1301@users.noreply.github.com>
2023-01-03 14:31:05 +0530

    blog-app-3

---
+++ blog-app-3/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+require("dotenv").config();
+const clc = require("cli-color");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const mongoDBSession = require("connect-mongodb-session")(session);
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+//file import
+const db = require("./db");
+const AuthRouter = require("./Controllers/Auth");
+
+//middlerwares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const store = new mongoDBSession({
+  uri: process.env.MONGODB_URL,
+  collection: "session",
+});
+
+app.use(
+  session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+//Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to blog app");
+});
+
+//routing
+app.use("/auth", AuthRouter);
+
+app.listen(PORT, () => {
+  console.log(clc.underline(`App is running at`));
+  console.log(clc.yellow(`http://localhost:${PORT}`));
+});
